f1e5f8fcfe1086b6ba7491158b2e59a9fff52da7
julius.suominen <julius.suominen@gmail.com>
2021-03-07 16:57:57 +0200

    add error handling example

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
+
+const port = ":8080"
 
 // home page handler
 func home(w http.ResponseWriter, r *http.Request) {
@@ -20,9 +23,30 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func divide(w http.ResponseWriter, r *http.Request) {
+	x, y := 100.0, 0.0
+	f, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
+}
+
+func divideValues(x, y float64) (float64, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero. ")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
+	http.HandleFunc("/divide", divide)
 
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", port))
+	_ = http.ListenAndServe(port, nil)
 }
