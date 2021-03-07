073a0e4eacbae691073a3b3cc65812497f0d4243
julius.suominen <julius.suominen@gmail.com>
2021-03-07 16:43:20 +0200

    add home, about

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
+// home page handler
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// about page handler
+func about(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, fmt.Sprintf("About page, sum is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
