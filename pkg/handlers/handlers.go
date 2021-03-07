2a6cf0539f46d0e2b3de967ee55c38179dd5ce93
julius.suominen <julius.suominen@gmail.com>
2021-03-07 18:24:07 +0200

    add template cache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,10 +8,10 @@ import (
 
 // Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,14 +4,61 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+// a map of custom functions that are needed in a template
+var functions = template.FuncMap{}
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+
+	_, err := RenderTemplateTest(w)
 	if err != nil {
-		fmt.Println("error parsin template:", err)
-		return
+		fmt.Println("Error getting template cache:", err)
 	}
+
+	/*
+		parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+		err := parsedTemplate.Execute(w, nil)
+		if err != nil {
+			fmt.Println("error parsing template:", err)
+			return
+		}
+	*/
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently:", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
